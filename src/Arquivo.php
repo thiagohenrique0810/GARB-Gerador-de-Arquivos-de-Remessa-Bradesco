a40ced93b63c300c56fc1624055ea1d14a51b724
Thiago <Thiago@PC>
2015-10-31 11:06:51 -0300

    Preparando classe de arquivo

--- src/Arquivo.php
+++ src/Arquivo.php
@@ -1,4 +1,35 @@
 <?php
 class Arquivo {
 	
+	/**
+	 * metodo para montar oarquivo
+	 */
+	private function monta_arquivo() {
+		//LOGICA AQUI
+	}
+	
+	/**
+	 * metodo para adicionar boletos na remessa
+	 * @param unknown $boleto
+	 */
+	public function add_boleto($boleto) {
+		//LOGICA AQUI
+	}
+	
+	/**
+	 * metodo para configurar a remessa
+	 * @param unknown $dados
+	 */
+	public function config($dados) {
+		//LOGICA AQUI
+	}
+	
+	/**
+	 * metoo para fazer download do arquivo de remessa
+	 */
+	public function save() {
+		//LOGICA AQUI
+	}
+	
+	
 }
